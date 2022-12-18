9aa5f0f80fefa90572c296b29dddd0df6a1bf5f4
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2022-12-18 16:42:49 -0300

    chore: change folder name

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { NavLink } from 'react-router-dom';
 import { Col, Row} from "react-bootstrap";
-import Musicollection from "../musicollection/musicollection"
+import albumcollection from "../albumcollection/albumcollection"
 import ZipCodeSearch from "../zip_code_search/zip_code_search"
 import Modal from "../common/modal/component";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -19,9 +19,9 @@ let Home = props => {
 		<Col lg={12}>
 			<Row>
 				<Col className="text-center hvr-grow item-panel mt-5" lg={3} md={3} sm={6} xs={12}>
-					<NavLink className="item-painel-link" to="/musicollection">
+					<NavLink className="item-painel-link" to="/albumcollection">
 						<FontAwesomeIcon icon={["fas", "fa-music"]} />
-						<p>music collection</p>
+						<p>album collection</p>
 					</NavLink>
 				</Col>
 				<Col className="text-center hvr-grow item-panel mt-5" lg={3} md={3} sm={6} xs={12}>
@@ -31,7 +31,7 @@ let Home = props => {
 					</NavLink>
 				</Col>
 			</Row>
-			<Modal title={"Collections"} subtitle={"Music"} content={<Musicollection />} handleClose={() => setShowMoat(false)} show={showModalMoat} />
+			<Modal title={"Collections"} subtitle={"Album"} content={<albumcollection />} handleClose={() => setShowMoat(false)} show={showModalMoat} />
 			<Modal title={"Search"} subtitle={"CEP"} content={<ZipCodeSearch />} handleClose={() => setShowLatech(false)} show={showModalLatech} />
 		</Col>
 	);
--- src/reducers/reducers.jsx
+++ src/reducers/reducers.jsx
@@ -1,14 +1,14 @@
 import { combineReducers } from "redux";
 import {reducer as toastr} from 'react-redux-toastr';
 import auth from "../auth/reducer";
-import musicollection from "../musicollection/reducer"
+import albumcollection from "../albumcollection/reducer"
 import zip_code_search from "../zip_code_search/reducer"
 
 import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   auth,
-  musicollection,
+  albumcollection,
   zip_code_search,
   form:formReducer,
   toastr
