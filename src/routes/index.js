0accc10369bfe9e060ab365e51efb3defcf3500b
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2022-12-18 16:42:57 -0300

    chore: change folder name

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 import Home from "../home/home";
-import Musicollection from "../musicollection/musicollection"
+import albumcollection from "../albumcollection/albumcollection"
 import ZipCodeSearch from "../zip_code_search/zip_code_search"
 
 var route = [
   { key: 1, exact: true, path: "/", component: Home },
-  { key: 2, exact: true, path: "/musicollection", component: Musicollection },
+  { key: 2, exact: true, path: "/albumcollection", component: albumcollection },
   { key: 3, exact: true, path: "/zip_code_search", component: ZipCodeSearch }
 ];
 
